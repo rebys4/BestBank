32cbcd3ddfa25944ff3ffe283dba3f18a6b7d6a2
rebys4 <tmdiasamidze@edu.hse.ru>
2025-04-28 20:08:59 +0300

    Add files

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,44 @@
+'use client';
+
 import Image from "next/image";
+import Link from "next/link";
+import Logo from "../public/logo.svg";
+import { Button } from "@headlessui/react";
+import RootLayout from "./layout";
+import { useState } from "react";
 
 export default function Home() {
+  const [accounts, setAccounts] = useState([
+    { name: 'Main Account', amount: 6700.56, highlighted: true },
+    { name: 'Expenses',     amount: 5134.63, highlighted: false },
+    { name: 'Savings',      amount: 36500.12, highlighted: false },
+  ])
+  const [value, setValue] = useState();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <main>
+        <section className="flex justify-center gap-[15px] mt-[25px] text-3xl font-bold font-Inter">
+          <Button className="w-[275px] h-[77px] bg-white rounded-[50px] border-[5px] border-[#FFA724] focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-400 data-open:bg-gray-400 cursor-pointer">Pay</Button>
+          <Button className="w-[275px] h-[77px] bg-white rounded-[50px] border-[5px] border-[#FFA724] focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-400 data-open:bg-gray-400 cursor-pointer">Transfer</Button>
+        </section>
+        <section className="flex justify-center flex-col items-center">
+          <h1 className="pr-[500px] mt-[42px] mb-[25px] font-bold font-Inter text-[40px]">Accounts</h1>
+          <ul className="flex flex-col gap-4">
+          {accounts.map(({ name, amount, highlighted }) => (
+            <li
+              key={name}
+              className={
+                `w-[600px] h-[100px] rounded-[10px] border-[5px] border-[#FFA724] flex items-center justify-between px-6` +
+                (highlighted ? ' bg-[#FCD59F]' : ' bg-white')
+              }
+            >
+              <span className="text-xl font-bold">{name}</span>
+              <span className="text-xl font-bold">
+                $ {amount.toLocaleString(undefined, { minimumFractionDigits: 2 })}
+              </span>
+            </li>
+          ))}
+        </ul>
+        </section>
+      </main>      
   );
 }
